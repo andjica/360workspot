01d10f6fce3680c4d51846adf4cc5945cbbb4f5c
andjica <andjaaa95@gmail.com>
2020-04-19 00:38:48 +0200

    adding middleware

--- routes/web.php
+++ routes/web.php
@@ -23,51 +23,47 @@ Route::get('/jobs-all', 'FrontController@jobsall')->name('jobs-all');
 
 Auth::routes();
 
+//user - job
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/users', 'HomeController@users')->name('users');
-Route::get('/user-buy/{id}', 'HomeController@userbought')->name('user-buy');
 Route::get('/tables', 'HomeController@tables')->name('tables');
-
-//job
-Route::get('/jobs-admin', 'JobController@index')->name('jobs-admin');
 Route::post('/create-job', 'JobController@create')->name('create-job');
 Route::get('/edit-job/{id}', 'JobController@edit')->name('edit-job');
 Route::post('/update-job/{id}', 'JobController@update')->name('update-job');
 
 
-//city
-Route::get('/cities', 'CityController@index')->name('cities');
-Route::get('/create-city', 'CityController@create')->name('create-city');
-Route::post('/insert-city', 'CityController@store')->name('insert-city');
-Route::get('/edit-city/{id}', 'CityController@edit')->name('edit-city');
-Route::post('/update-city/{id}', 'CityController@update')->name('update-city');
-Route::get('/delete-city/{id}', 'CityController@destroy')->name('delete-city');
+Route::group(['middleware' => ['checkaccount']], function () 
+{
+    //admin jobs
+    Route::get('/jobs-admin', 'JobController@index')->name('jobs-admin');
 
-//category
-Route::get('/categories', 'CategoryController@index')->name('categories');
-Route::get('/create-category', 'CategoryController@create')->name('create-category');
-Route::post('/insert-category', 'CategoryController@store')->name('insert-category');
-Route::get('/edit-category/{id}', 'CategoryController@edit')->name('edit-category');
-Route::post('/update-category/{id}', 'CategoryController@update')->name('update-category');
-Route::get('/delete-category/{id}', 'CategoryController@destroy')->name('delete-category');
+    //admin users
+    Route::get('/users', 'HomeController@users')->name('users');
+    Route::get('/user-buy/{id}', 'HomeController@userbought')->name('user-buy');
+    //city
+    Route::get('/cities', 'CityController@index')->name('cities');
+    Route::get('/create-city', 'CityController@create')->name('create-city');
+    Route::post('/insert-city', 'CityController@store')->name('insert-city');
+    Route::get('/edit-city/{id}', 'CityController@edit')->name('edit-city');
+    Route::post('/update-city/{id}', 'CityController@update')->name('update-city');
+    Route::get('/delete-city/{id}', 'CityController@destroy')->name('delete-city');
 
-//blog
-Route::get('/blogs', 'BlogController@index')->name('blogs');
-Route::get('/blog/{id}', 'BlogController@show')->name('blog-one');
-Route::get('/create-blog', 'BlogController@create')->name('create-blog');
-Route::post('/insert-blog', 'BlogController@store')->name('insert-blog');
-Route::get('/edit-blog/{id}', 'BlogController@edit')->name('edit-blog');
-Route::post('/update-blog/{id}','BlogController@update')->name('update-blog');
-Route::get('/delete-blog/{id}', 'BlogController@destroy')->name('delete-blog');
+    //category
+    Route::get('/categories', 'CategoryController@index')->name('categories');
+    Route::get('/create-category', 'CategoryController@create')->name('create-category');
+    Route::post('/insert-category', 'CategoryController@store')->name('insert-category');
+    Route::get('/edit-category/{id}', 'CategoryController@edit')->name('edit-category');
+    Route::post('/update-category/{id}', 'CategoryController@update')->name('update-category');
+    Route::get('/delete-category/{id}', 'CategoryController@destroy')->name('delete-category');
 
-
-//for email - apply for a job and contact
-Route::post('/email-apply', 'EmailController@send')->name('email-apply');
-Route::post('/email-contact', 'EmailController@contact')->name('email-contact');
-Route::get('/user-contact', 'HomeController@usercontact')->name('user-contact');
-Route::get('/contact', 'FrontController@contact')->name('contact');
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
-
+    //blog
+    Route::get('/blogs', 'BlogController@index')->name('blogs');
+    Route::get('/blog/{id}', 'BlogController@show')->name('blog-one');
+    Route::get('/create-blog', 'BlogController@create')->name('create-blog');
+    Route::post('/insert-blog', 'BlogController@store')->name('insert-blog');
+    Route::get('/edit-blog/{id}', 'BlogController@edit')->name('edit-blog');
+    Route::post('/update-blog/{id}','BlogController@update')->name('update-blog');
+    Route::get('/delete-blog/{id}', 'BlogController@destroy')->name('delete-blog');
+});
 
 //paypal routes
 Route::post('/upgrade-pro', 'PaymentController@paypro')->name('upgrade-pro'); 
@@ -77,5 +73,12 @@ Route::post('/upgrade-exclusive', 'PaymentController@payexclusive')->name('upgra
 Route::get('/status-pro', 'PaymentController@getPaymentStatusPro'); 
 Route::get('/status-professional', 'PaymentController@getPaymentStatusProfessional');
 Route::get('/status-excl', 'PaymentController@getPaymentStatusExclusive');
+
+//for email - apply for a job and contact
+Route::post('/email-apply', 'EmailController@send')->name('email-apply');
+Route::post('/email-contact', 'EmailController@contact')->name('email-contact');
+Route::get('/user-contact', 'HomeController@usercontact')->name('user-contact');
+Route::get('/contact', 'FrontController@contact')->name('contact');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 
