33f0ae5ad875ac7f9fff8b0dced5867be8b49727
andjica <andjaaa95@gmail.com>
2020-05-24 01:28:36 +0200

    new middleware :)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkaccount' => \App\Http\Middleware\CheckAccount::class,
+        'checkcompany' => \App\Http\Middleware\CheckCompany::class,
     ];
 
     /**
